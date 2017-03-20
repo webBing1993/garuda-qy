bb612248765127ffeb2810096b4a7204d0caa350
GongTianyi <ted@>
2017-03-20 16:41:50 +0800

    normalize.css & main.less

--- src/main.js
+++ src/main.js
@@ -5,6 +5,10 @@ import App from './App'
 
 Vue.config.productionTip = false
 
+// normalize.css & main.less
+import 'normalize.css'
+import './assets/main.less'
+
 // router
 import router from './router'
 
