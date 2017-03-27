6af4e801b79d78caa5869895e5df759b62895545
lisj <lisj@fortrun.cn>
2017-03-27 18:57:40 +0800

    up

--- src/components/calendar/index.vue
+++ src/components/calendar/index.vue
@@ -1,25 +1,29 @@
 <template>
+
   <div class="calendar">
-    <p>{{current}}</p>
-    <p>Start:{{starttime}}</p>
-    <p>End:{{endtime}}</p>
+    <div class="mask"></div>
+    <div class="container">
+      <p>{{current}}</p>
+      <p>Start:{{starttime}}</p>
+      <p>End:{{endtime}}</p>
 
-    <div class="tools-bar">
-      <span class="clear" @click="(starttime=null,endtime=null)">清除筛选</span>
-      <span class="cancel">取消</span>
-    </div>
+      <div class="tools-bar">
+        <span class="clear" @click="(starttime=null,endtime=null)">清除筛选</span>
+        <span class="cancel">取消</span>
+      </div>
 
-    <div class="select-bar">
-      <span @click="nextMonth(-1)">左</span>
-      <span>{{year}}年{{month + 1}}月</span>
-      <span @click="nextMonth(1)">右</span>
-    </div>
-    <div class="day-list">
+      <div class="select-bar">
+        <span @click="nextMonth(-1)">左</span>
+        <span>{{year}}年{{month + 1}}月</span>
+        <span @click="nextMonth(1)">右</span>
+      </div>
+      <div class="day-list">
       <span v-for="item in renderdays"
             @click="dateclick(item)"
             class="day-item"
             :class="{checked:item.checked}"
       >{{item.day}}</span>
+      </div>
     </div>
 
   </div>
