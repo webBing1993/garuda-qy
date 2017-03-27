a1cec28582fa53945b84533d88257de0b86afe29
lisj <lisj@fortrun.cn>
2017-03-27 17:01:57 +0800

    Up

--- src/components/calendar/index.vue
+++ src/components/calendar/index.vue
@@ -1,21 +1,29 @@
 <template>
-  <div>
-    <h1>calendar  </h1>
+  <div class="calendar">
+    <p>{{current}}</p>
     <p>{{starttime}}</p>
     <p>{{endtime}}</p>
-    <div v-for="item in daylist" @click="dateclick(item)">{{item.day}} {{item.checked}} {{item.disabled}}</div>
+    <div class="select-bar">
+      <span>左</span>
+      <span>{{year}}年{{month+1}}月</span>
+      <span @click="nextMonth(1)">右</span>
+    </div>
+    <div class="day-list">
+      <span v-for="item in daylist" @click="dateclick(item)" class="day-item">{{item.day}}</span>
+    </div>
+
   </div>
 </template>
 
 <script>
   import moment from 'moment'
   moment.locale('zh-cn');
-  console.log(moment.locale())
+
   export default{
     name: 'calendar',
     data(){
       return {
-        current: moment(),
+        current: moment(new Date()),
         starttime: null,
         endtime: null,
       }
@@ -60,6 +68,17 @@
         } else {
           this.starttime = null
           this.endtime = null
+        }
+      },
+      nextMonth(val) {
+        console.log('0')
+        if (val > 0) {
+//          console.log(this.current)
+//          let a = Object.assign(this.current)
+//          a.add(1, 'months')
+//          console.log(a.toLocaleString())
+//          this.current = a
+//          this.current = this.current.add(val, 'months');
         }
       }
     }
