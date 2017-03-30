22d0e4d188d3d1a2fc28f20173866f3dc4a77eac
GongTianyi <ted@>
2017-03-30 17:06:51 +0800

    update example

--- src/containers/home.vue
+++ src/containers/home.vue
@@ -1,81 +1,75 @@
 <template>
-  <div class="home">
-    <h1>Forms</h1>
-    <Group title="XInput">
-      <XInput title="手机号"/>
-      <XInput type="number" title="手机号" v-model="inputValue" placeholder="请输入您的常用手机号"/>
-      <XInput fill/>
-    </Group>
-    <Group title="XTextarea">
-      <XTextarea v-model="textareaValue" max="50"/>
-    </Group>
-    <Group title="Switch">
-      <XSwitch title="我是开关" v-model="switchValue"/>
-      <XSwitch title="我是开关" v-model="switchValue"/>
-    </Group>
-    <Group radio title="XRadio">
-      <XRadio :options="[1,2,3]" v-model="radioValue"/>
-    </Group>
-    <Group checkbox title="XCheckbox">
-      <XCheckbox :options="[1,2,3]" v-model="checklistValue"/>
-    </Group>
+  <div>
+    <scroller lock-x :scrollbar-x=false use-pulldown height="-44">
+      <div>
+        <h1>Forms</h1>
+        <Group title="XInput">
+          <XInput title="手机号"/>
+          <XInput type="number" title="手机号" v-model="inputValue" placeholder="请输入您的常用手机号"/>
+          <XInput fill/>
+        </Group>
+        <Group title="XTextarea">
+          <XTextarea v-model="textareaValue" max="50"/>
+        </Group>
+        <Group title="Switch">
+          <XSwitch title="我是开关" v-model="switchValue"/>
+          <XSwitch title="我是开关" v-model="switchValue"/>
+        </Group>
+        <Group radio title="XRadio">
+          <XRadio :options="[1,2,3]" v-model="radioValue"/>
+        </Group>
+        <Group checkbox title="XCheckbox">
+          <XCheckbox :options="[1,2,3]" v-model="checklistValue"/>
+        </Group>
 
-    <h1>Layers</h1>
-    <Group title="Popup">
-      <XSwitch title="Popup" v-model="popupShow"/>
-      <popup v-model="popupShow"
-             :maskShow="true"
-             :bottom="false"
-             :top="false"
-             :center="true"
-             :Animation="true">
-        <calendar @onCancel="popupShow= false"></calendar>
-      </popup>
-    </Group>
-    <Group title="Actionsheet">
-      <XSwitch title="Actionsheet" v-model="showAction"/>
-      <Actionsheet v-model="showAction"/>
-    </Group>
-    <Group title="Dialog">
-      <XSwitch title="Dialog" v-model="showDialog"/>
-      <Dialog v-model="showDialog" confirm title="弹窗呀呀呀呀"/>
-    </Group>
-    <Group title="Toast">
-      <XSwitch title="Toast 2s" v-model="showToast"/>
-      <XSwitch title="Loading" v-model="showLoading"/>
-      <Toast v-model="showToast"/>
-      <Toast v-model="showLoading" loading/>
-    </Group>
+        <h1>Layers</h1>
+        <Group title="Actionsheet">
+          <XSwitch title="Actionsheet" v-model="showAction"/>
+        </Group>
+        <Group title="Dialog">
+          <XSwitch title="Dialog" v-model="showDialog"/>
+        </Group>
+        <Group title="Toast">
+          <XSwitch title="Toast 2s" v-model="showToast"/>
+          <XSwitch title="Loading" v-model="showLoading"/>
 
-    <h1>List</h1>
-    <Group title="Group Cell Link Badge Dot">
-      <Cell title="手机号"/>
-      <Cell
-        icon="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAC4AAAAuCAMAAABgZ9sFAAAAVFBMVEXx8fHMzMzr6+vn5+fv7+/t7e3d3d2+vr7W1tbHx8eysrKdnZ3p6enk5OTR0dG7u7u3t7ejo6PY2Njh4eHf39/T09PExMSvr6+goKCqqqqnp6e4uLgcLY/OAAAAnklEQVRIx+3RSRLDIAxE0QYhAbGZPNu5/z0zrXHiqiz5W72FqhqtVuuXAl3iOV7iPV/iSsAqZa9BS7YOmMXnNNX4TWGxRMn3R6SxRNgy0bzXOW8EBO8SAClsPdB3psqlvG+Lw7ONXg/pTld52BjgSSkA3PV2OOemjIDcZQWgVvONw60q7sIpR38EnHPSMDQ4MjDjLPozhAkGrVbr/z0ANjAF4AcbXmYAAAAASUVORK5CYII="
-        title="手机号"
-        value="15300686668"
-        link/>
-      <Link value="查看更多"/>
-      <Cell
-        icon="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAC4AAAAuCAMAAABgZ9sFAAAAVFBMVEXx8fHMzMzr6+vn5+fv7+/t7e3d3d2+vr7W1tbHx8eysrKdnZ3p6enk5OTR0dG7u7u3t7ejo6PY2Njh4eHf39/T09PExMSvr6+goKCqqqqnp6e4uLgcLY/OAAAAnklEQVRIx+3RSRLDIAxE0QYhAbGZPNu5/z0zrXHiqiz5W72FqhqtVuuXAl3iOV7iPV/iSsAqZa9BS7YOmMXnNNX4TWGxRMn3R6SxRNgy0bzXOW8EBO8SAClsPdB3psqlvG+Lw7ONXg/pTld52BjgSSkA3PV2OOemjIDcZQWgVvONw60q7sIpR38EnHPSMDQ4MjDjLPozhAkGrVbr/z0ANjAF4AcbXmYAAAAASUVORK5CYII="
-        title="手机号"
-        desc="123"
-        badge="New"/>
-      <Cell title="dot" value="true" dot/>
-      <Cell title="badge" value="详细信息" badge="8" link/>
-      <Cell title="badge" value="详细信息" badge="New" link/>
-    </Group>
+        </Group>
 
-    <h1>Layouts</h1>
-    <Grids>
-      <Grid value="1" img="123"/>
-      <Grid value="2" img="123"/>
-      <Grid value="3" img="123"/>
-    </Grids>
+        <h1>List</h1>
+        <Group title="Group Cell Link Badge Dot">
+          <Cell title="手机号"/>
+          <Cell
+            icon="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAC4AAAAuCAMAAABgZ9sFAAAAVFBMVEXx8fHMzMzr6+vn5+fv7+/t7e3d3d2+vr7W1tbHx8eysrKdnZ3p6enk5OTR0dG7u7u3t7ejo6PY2Njh4eHf39/T09PExMSvr6+goKCqqqqnp6e4uLgcLY/OAAAAnklEQVRIx+3RSRLDIAxE0QYhAbGZPNu5/z0zrXHiqiz5W72FqhqtVuuXAl3iOV7iPV/iSsAqZa9BS7YOmMXnNNX4TWGxRMn3R6SxRNgy0bzXOW8EBO8SAClsPdB3psqlvG+Lw7ONXg/pTld52BjgSSkA3PV2OOemjIDcZQWgVvONw60q7sIpR38EnHPSMDQ4MjDjLPozhAkGrVbr/z0ANjAF4AcbXmYAAAAASUVORK5CYII="
+            title="手机号"
+            value="15300686668"
+            link/>
+          <Link value="查看更多"/>
+          <Cell
+            icon="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAC4AAAAuCAMAAABgZ9sFAAAAVFBMVEXx8fHMzMzr6+vn5+fv7+/t7e3d3d2+vr7W1tbHx8eysrKdnZ3p6enk5OTR0dG7u7u3t7ejo6PY2Njh4eHf39/T09PExMSvr6+goKCqqqqnp6e4uLgcLY/OAAAAnklEQVRIx+3RSRLDIAxE0QYhAbGZPNu5/z0zrXHiqiz5W72FqhqtVuuXAl3iOV7iPV/iSsAqZa9BS7YOmMXnNNX4TWGxRMn3R6SxRNgy0bzXOW8EBO8SAClsPdB3psqlvG+Lw7ONXg/pTld52BjgSSkA3PV2OOemjIDcZQWgVvONw60q7sIpR38EnHPSMDQ4MjDjLPozhAkGrVbr/z0ANjAF4AcbXmYAAAAASUVORK5CYII="
+            title="手机号"
+            desc="123"
+            badge="New"/>
+          <Cell title="dot" value="true" dot/>
+          <Cell title="badge" value="详细信息" badge="8" link/>
+          <Cell title="badge" value="详细信息" badge="New" link/>
+        </Group>
 
-    <h1>Display</h1>
-    <XButton value="Vue2" primary warn/>
+        <h1>Layouts</h1>
+        <Grids>
+          <Grid value="1" img="123"/>
+          <Grid value="2" img="123"/>
+          <Grid value="3" img="123"/>
+        </Grids>
 
+        <h1>Display</h1>
+        <XButton value="Vue2" primary warn/>
+      </div>
+    </scroller>
+
+    <Actionsheet v-model="showAction"/>
+    <Dialog v-model="showDialog" confirm title="弹窗呀呀呀呀"/>
+    <Toast v-model="showToast"/>
+    <Toast v-model="showLoading" loading/>
   </div>
 </template>
 
