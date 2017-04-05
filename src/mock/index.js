5aeeaaba3376bd1c33c814c110f1e39f170ca3d0
GongTianyi <ted@>
2017-04-05 17:53:20 +0800

    mock

--- src/mock/index.js
+++ src/mock/index.js
@@ -43,7 +43,7 @@ Mock.mock(
       timeline: { //状态变化时间
         precheckin_done: null, //预登记完成时间, null未发生
       }
-    },{
+    }, {
       order_id: "230420402402404",//订单号
       in_time: '',
       out_time: '',
@@ -69,7 +69,7 @@ Mock.mock(
       timeline: { //状态变化时间
         precheckin_done: null, //预登记完成时间, null未发生
       }
-    },{
+    }, {
       order_id: "230420402402404",//订单号
       in_time: '',
       out_time: '',
@@ -166,7 +166,7 @@ Mock.mock(
       timeline: { //状态变化时间
         precheckin_done: null, //预登记完成时间, null未发生
       }
-    },{
+    }, {
       order_id: "230420402402404",//订单号
       in_time: '',
       out_time: '',
@@ -192,7 +192,7 @@ Mock.mock(
       timeline: { //状态变化时间
         precheckin_done: null, //预登记完成时间, null未发生
       }
-    },{
+    }, {
       order_id: "230420402402404",//订单号
       in_time: '',
       out_time: '',
@@ -467,3 +467,220 @@ Mock.mock(
     }]
   }
 );
+
+
+// 房态管理
+Mock.mock(
+  prefix + '/room/room_type_list',
+  {
+    errcode: "0",
+    errmsg: "ok",
+    data: [
+      {
+        room_type_id: 'DCF',
+        room_type_name: '大床房'
+      }, {
+        room_type_id: 'SCF',
+        room_type_name: '双床房'
+      }
+    ]
+  }
+)
+
+Mock.mock(
+  prefix + '/room/floor_list',
+  {
+    errcode: "0",
+    errmsg: "ok",
+    data: [
+      {
+        floor_id: 111,
+        floor_name: '1层'
+      }, {
+        floor_id: 222,
+        floor_name: '2层'
+      }, {
+        floor_id: 333,
+        floor_name: '3层'
+      }, {
+        floor_id: 444,
+        floor_name: '4层'
+      }
+    ]
+  }
+)
+
+Mock.mock(
+  prefix + '/room/list',
+  {
+    errcode: "0",
+    errmsg: "ok",
+    data: [
+      {
+        room_id: 'f101',//房间id
+        room_number: 101,//房间号
+        room_status: 0, //0:空净 1:空脏 2:在住
+        room_type_id: 'DCF', //房型代码
+        floor_id: 111, //楼层id
+        config: {
+          enabled_pick: true, //是否允许自助选房
+        },
+        owner_name: 'ted',
+        guest_name: ''
+      }, {
+        room_id: 'f102',//房间id
+        room_number: '102',//房间号
+        room_status: 0, //0:空净 1:空脏 2:在住
+        room_type_id: 'DCF', //房型代码
+        floor_id: 111, //楼层id
+        config: {
+          enabled_pick: true, //是否允许自助选房
+        },
+        owner_name: '',
+        guest_name: ''
+      }, {
+        room_id: 'f103',//房间id
+        room_number: '103',//房间号
+        room_status: 0, //0:空净 1:空脏 2:在住
+        room_type_id: 'DCF', //房型代码
+        floor_id: 111, //楼层id
+        config: {
+          enabled_pick: true, //是否允许自助选房
+        },
+        owner_name: '',
+        guest_name: ''
+      }, {
+        room_id: 'f104',//房间id
+        room_number: '104',//房间号
+        room_status: 0, //0:空净 1:空脏 2:在住
+        room_type_id: 'DCF', //房型代码
+        floor_id: 111, //楼层id
+        config: {
+          enabled_pick: true, //是否允许自助选房
+        },
+        owner_name: '',
+        guest_name: ''
+      }, {
+        room_id: 'f105',//房间id
+        room_number: '105',//房间号
+        room_status: 2, //0:空净 1:空脏 2:在住
+        room_type_id: 'DCF', //房型代码
+        floor_id: 111, //楼层id
+        config: {
+          enabled_pick: true, //是否允许自助选房
+        },
+        owner_name: '思路',
+        guest_name: ''
+      }, {
+        room_id: 'f106',//房间id
+        room_number: '106',//房间号
+        room_status: 2, //0:空净 1:空脏 2:在住
+        room_type_id: 'DCF', //房型代码
+        floor_id: 111, //楼层id
+        config: {
+          enabled_pick: true, //是否允许自助选房
+        },
+        owner_name: '思路',
+        guest_name: '思路'
+      }, {
+        room_id: 'f201',//房间id
+        room_number: '201',//房间号
+        room_status: 1, //0:空净 1:空脏 2:在住
+        room_type_id: 'SCF', //房型代码
+        floor_id: 222, //楼层id
+        config: {
+          enabled_pick: true, //是否允许自助选房
+        },
+        owner_name: 'ted',
+        guest_name: 'alan'
+      }, {
+        room_id: 'f202',//房间id
+        room_number: '202',//房间号
+        room_status: 1, //0:空净 1:空脏 2:在住
+        room_type_id: 'SCF', //房型代码
+        floor_id: 222, //楼层id
+        config: {
+          enabled_pick: false, //是否允许自助选房
+        },
+        owner_name: '',
+        guest_name: ''
+      }, {
+        room_id: 'f203',//房间id
+        room_number: '203',//房间号
+        room_status: 1, //0:空净 1:空脏 2:在住
+        room_type_id: 'SCF', //房型代码
+        floor_id: 222, //楼层id
+        config: {
+          enabled_pick: false, //是否允许自助选房
+        },
+        owner_name: '',
+        guest_name: ''
+      }, {
+        room_id: 'f301',//房间id
+        room_number: '301',//房间号
+        room_status: 2, //0:空净 1:空脏 2:在住
+        room_type_id: 'SCF', //房型代码
+        floor_id: 333, //楼层id
+        config: {
+          enabled_pick: false, //是否允许自助选房
+        },
+        owner_name: 'ted',
+        guest_name: 'alan'
+      }, {
+        room_id: 'f302',//房间id
+        room_number: '302',//房间号
+        room_status: 2, //0:空净 1:空脏 2:在住
+        room_type_id: 'SCF', //房型代码
+        floor_id: 333, //楼层id
+        config: {
+          enabled_pick: false, //是否允许自助选房
+        },
+        owner_name: 'ted',
+        guest_name: 'alan'
+      }, {
+        room_id: 'f303',//房间id
+        room_number: '303',//房间号
+        room_status: 2, //0:空净 1:空脏 2:在住
+        room_type_id: 'SCF', //房型代码
+        floor_id: 333, //楼层id
+        config: {
+          enabled_pick: false, //是否允许自助选房
+        },
+        owner_name: 'ted',
+        guest_name: 'alan'
+      }
+    ]
+  }
+)
+
+Mock.mock(
+  prefix + '/room/status',
+  {
+    errcode: "0",
+    errmsg: "ok"
+  }
+)
+
+Mock.mock(
+  prefix + '/room/enabled_pick',
+  {
+    errcode: "0",
+    errmsg: "ok"
+  }
+)
+
+Mock.mock(
+  prefix + '/room/inout',
+  {
+    errcode: "0",
+    errmsg: "ok"
+  }
+)
+
+Mock.mock(
+  prefix + '/room/cancel_suborder',
+  {
+    errcode: "0",
+    errmsg: "ok"
+  }
+)
