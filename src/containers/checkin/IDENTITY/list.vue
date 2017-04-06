bbc28e6649e5326a72972856ee7d35f7e692ad73
GongTianyi <ted@>
2017-04-06 15:25:41 +0800

    目录调整，增加IDENTITY

---
+++ src/containers/checkin/IDENTITY/list.vue
@@ -0,0 +1,32 @@
+<template>
+  <article>
+    <Tab>
+      <TabItem v-for="(item,index) in tabMenu"
+               :value="item"
+               :key="index"
+               :selected="selectedTab === item"
+               @onSelected="selectedTab = item"/>
+    </Tab>
+  </article>
+</template>
+
+<script>
+  import {mapState, mapGetters, mapActions, mapMutations} from 'vuex';
+
+  module.exports = {
+    name: 'identity-list',
+    data(){
+      return {
+        tabMenu: ['已通过', '未通过'],
+        selectedTab: '已通过'
+      }
+    },
+    computed: {
+      isToday(){
+        return this.$route.path.indexOf('today') > -1
+      }
+    },
+    methods: {}
+  }
+</script>
+
