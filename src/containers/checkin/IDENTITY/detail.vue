c2cac48e78f1f4562e411bd11e805fd12478b2d6
lisj <lisj@fortrun.cn>
2017-04-28 18:09:40 +0800

    identity

--- src/containers/checkin/IDENTITY/detail.vue
+++ src/containers/checkin/IDENTITY/detail.vue
@@ -1,41 +1,37 @@
 <template>
   <article>
-    <scroller lock-x :depend="detail">
-      <div class="scroller-wrap">
-        <Group>
-          <Cell title="预订人" :value="detail.owner"></Cell>
-          <Cell title="手机号" :value="detail.phone_number"></Cell>
-          <Cell title="入离日期" :value="datetimeparse(detail.in_time)+'-'+datetimeparse(detail.out_time)"></Cell>
-          <Cell title="房间" v-if="detail.room" :value="detail.room.room_type_name + detail.room.room_number"></Cell>
-        </Group>
+    <Group>
+      <Cell title="预订人" :value="detail.owner"></Cell>
+      <Cell title="手机号" :value="detail.phone_number"></Cell>
+      <Cell title="入离日期" :value="datetimeparse(detail.in_time)+'-'+datetimeparse(detail.out_time)"></Cell>
+      <Cell title="房间" v-if="detail.room" :value="detail.room.room_type_name + detail.room.room_number"></Cell>
+    </Group>
 
-        <div class="guestcard" v-for="item in detail.guests">
-          <div class="hd">
-            <ul>
-              <li><span><abbr>姓名</abbr>{{item.name}}</span></li>
-              <li><span><abbr>性别</abbr>{{item.sex}}</span><span><abbr>民族</abbr>{{item.ethnicity}}</span></li>
-              <li><span><abbr>生日</abbr>{{item.date_of_birth}}</span></li>
-              <li><span><abbr>住址</abbr>{{item.address}}</span></li>
-              <li><span><abbr>身份证号</abbr>{{idnumber(item.idcard)}}</span></li>
-            </ul>
-            <img :src="item.photo" alt="身份证照片">
-          </div>
-          <div class="bd">
-            <p><span>现场图片</span><span>相似度： <abbr>{{item.similarity}}%</abbr></span></p>
-            <img :src="item.live_photo" alt="现场照片">
-          </div>
-        </div>
-        <p v-if="!isUploadSuccess" class="upload"> 上传旅业系统失败，请重试</p>
-        <p v-if="isUploadSuccess" class="upload-time">
-          已成功上传旅业系统。{{datetimeparse(detail.lvye_report_time, 'YYMMDD hhmm')}}</p>
-        <div class="button-group">
-          <x-button v-if="!btnPresent.status || btnPresent.hasNext"
-                    :value="btnPresent.status ? '下一个' : '上传旅业系统'"
-                    :plain="!!btnPresent.status"
-                    @onClick="btnPresent.callback"></x-button>
-        </div>
+    <div class="guestcard" v-for="item in detail.guests">
+      <div class="hd">
+        <ul>
+          <li><span><abbr>姓名</abbr>{{item.name}}</span></li>
+          <li><span><abbr>性别</abbr>{{item.sex}}</span><span><abbr>民族</abbr>{{item.ethnicity}}</span></li>
+          <li><span><abbr>生日</abbr>{{item.date_of_birth}}</span></li>
+          <li><span><abbr>住址</abbr>{{item.address}}</span></li>
+          <li><span><abbr>身份证号</abbr>{{idnumber(item.idcard)}}</span></li>
+        </ul>
+        <img :src="item.photo" alt="身份证照片">
       </div>
-    </scroller>
+      <div class="bd">
+        <p><span>现场图片</span><span>相似度： <abbr>{{item.similarity}}%</abbr></span></p>
+        <img :src="item.live_photo" alt="现场照片">
+      </div>
+    </div>
+    <p v-if="!isUploadSuccess" class="upload"> 上传旅业系统失败，请重试</p>
+    <p v-if="isUploadSuccess" class="upload-time">
+      已成功上传旅业系统。{{datetimeparse(detail.lvye_report_time, 'YYMMDD hhmm')}}</p>
+    <div class="button-group">
+      <x-button v-if="!btnPresent.status || btnPresent.hasNext"
+                :value="btnPresent.status ? '下一个' : '上传旅业系统'"
+                :plain="!!btnPresent.status"
+                @onClick="btnPresent.callback"></x-button>
+    </div>
   </article>
 </template>
 
