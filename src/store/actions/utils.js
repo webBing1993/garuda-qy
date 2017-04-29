71b3f0a460e15a8fe1eb8cd985b4d43248df3c81
Wills <wills@fortrun.cn>
2017-04-29 15:05:26 +0800

    [Wills] - 请求超时由5秒调升为10秒。

--- src/store/actions/utils.js
+++ src/store/actions/utils.js
@@ -28,7 +28,7 @@ module.exports = {
       },
       params: param.params || null,
       method: param.method || "GET",
-      timeout: param.timeout || 5000,
+      timeout: param.timeout || 10000,
       credentials: false,
       emulateHTTP: false,
       emulateJSON: false,
