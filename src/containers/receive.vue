53c281e5a04f01e24e1b65efd3dd6c76501f02e6
lisj <lisj@fortrun.cn>
2017-09-20 18:00:17 +0800

    bug fixed

--- src/containers/receive.vue
+++ src/containers/receive.vue
@@ -269,13 +269,13 @@
           this.getcheckoutlist({
             status: 'PENDING',
             start_time: this.periodFilter[0],
-            end_time: this.periodFilter[0] == this.periodFilter[1] ? this.periodFilter[1] + 86400000 : this.periodFilter[1],
+            end_time: this.periodFilter[1] ? this.periodFilter[0] == this.periodFilter[1] ? this.periodFilter[1] + 86400000 : this.periodFilter[1]:'',
             onsuccess: body => (this.checkOutApplicationList = [...body.data], this.checkOutApplicationPageIndex++)
           });
           this.getcheckoutlist({
             status: 'DONE',
             start_time: this.periodFilter[0],
-            end_time: this.periodFilter[0] == this.periodFilter[1] ? this.periodFilter[1] + 86400000 : this.periodFilter[1],
+            end_time: this.periodFilter[1] ? this.periodFilter[0] == this.periodFilter[1] ? this.periodFilter[1] + 86400000 : this.periodFilter[1] :'',
             onsuccess: body => (this.checkOutList = [...body.data], this.checkoutPageIndex++)
           });
         }
