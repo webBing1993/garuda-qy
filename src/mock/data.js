869c7a1ac6c05b0c09d1e246359c109acab38246
lisj <lisj@fortrun.cn>
2017-10-11 11:41:51 +0800

    旅业上传按钮去掉，跳转到入住核验

--- src/mock/data.js
+++ src/mock/data.js
@@ -160,7 +160,7 @@ module.exports = {
           out_time: 1490976000000, //离店时间
           identity_id: "", //身份验证记录ID:null,
           lvye_report_time: 1490976000000, //成功上传旅业系统的时间时间戳
-          lvye_report_status: 'SUCCESS', //NONE未上传，SUCCESS成功，FAILED失败, PENDING等待 。上传旅业系统状态。
+          lvye_report_status: 'FAILED', //NONE未上传，SUCCESS成功，FAILED失败, PENDING等待 。上传旅业系统状态。
           pmscheckout_status: "PENDING"//PENDING处理中, SUCCESS退房成功,FAILED退房失败'
         }, {
           suborder_id: "2304204024024011",
