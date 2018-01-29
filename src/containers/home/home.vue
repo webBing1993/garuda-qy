75aaee106cd4e4294c66617ca6de602db99194b4
Zoe <zoe.zheng@fortrun.cn>
2018-01-29 20:31:26 +0800

    [zoe]首页样式对调

--- src/containers/home/home.vue
+++ src/containers/home/home.vue
@@ -1,64 +1,56 @@
 <template>
   <div class="home-container">
-
-    <Group title="待办事项" v-if="isHaveTodoList">
-
-      <Cell v-if="prepayTodoNum > 0" icon="../../../static/icon/ic_prepay_confirm.png" title="订单金额待确认" link :badge="prepayTodoNum"
-            @onClick="goto('prepay/0')"></Cell>
-      <Cell v-if="identityNum > 0" icon="../../../static/icon/ic_police.png" title="您有一条公安验证待处理" link :badge="identityNum"
-            @onClick="goto('/identity/0')"></Cell>
-      <Cell v-if="policeIdentityNum > 0" icon="../../../static/icon/ic_lvye.png" title="旅业信息待上传" link :badge="policeIdentityNum"
-            @onClick="goto('policeIdentity/handle/0')"></Cell>
-      <Cell v-if="invoiceNum > 0" icon="../../../static/icon/ic_invoice.png" title="开票申请待处理" link :badge="invoiceNum"
-            @onClick="goto('invoice/0')"></Cell>
-      <Cell v-if="checkoutApplicationNum > 0" icon="../../../static/icon/ic_checkout.png" title="离店申请待处理" link :badge="checkoutApplicationNum"
-            @onClick="goto('receive/checkout-application')"></Cell>
-      <Cell v-if="abnormalNoticeNum > 0" icon="../../../static/icon/ic_abnormity_notice.png" title="异常提醒" link :badge="abnormalNoticeNum"
-            @onClick="goto('abnormalNotice/listDetail')"></Cell>
-    </Group>
-
-    <div v-else class="none-list-container">
-      <img src="../../../static/icon/no_todo_list.png" alt="">
-      <span>暂无待办事项</span>
-    </div>
-
     <div class="app">
       <Group title="应用">
-        <ul class="app-container">
+        <ul class="app-container" >
+          <li class="app-item" @click="goto('/policeIdentity/handle/0')">
+            <img :src='flag?imgList[0].openItem:imgList[0].closeItem' alt="身份核验">
+            <span class="app-title">公安验证</span>
+          </li>
           <li class="app-item" @click="goto('prepay/0')">
-            <img src="../../../static/icon/ic_prepay_confirm.png" alt="订单中心">
+            <img :src='flag?imgList[1].openItem:imgList[1].closeItem' alt="订单中心">
             <span class="app-title">订单中心</span>
           </li>
           <li class="app-item" @click="goto('receive/precheckin')">
-            <img src="../../../static/icon/ic_checkout.png" alt="接待服务">
+            <img :src='flag?imgList[2].openItem:imgList[2].closeItem' alt="接待服务">
             <span class="app-title">住离信息</span>
           </li>
           <li class="app-item" @click="goto('/identity/0')">
-            <img src="../../../static/icon/ic_police.png" alt="公安验证">
+            <img :src='flag?imgList[3].openItem:imgList[3].closeItem' alt="公安验证">
             <span class="app-title">入住核验</span>
           </li>
-          <li class="app-item" @click="goto('/policeIdentity/handle/0')">
-            <img src="../../../static/icon/ic_lvye.png" alt="身份核验">
-            <span class="app-title">设备核验</span>
-          </li>
           <li class="app-item" @click="goto('/invoice/0')">
-            <img src="../../../static/icon/ic_invoice.png" alt="闪开发票">
+            <img :src='flag?imgList[4].openItem:imgList[4].closeItem' alt="闪开发票">
             <span class="app-title">发票中心</span>
           </li>
           <li class="app-item" @click="goto('/bill/0')">
-            <img src="../../../static/icon/ic_bill.png" alt="账务管理">
+            <img :src='flag?imgList[5].openItem:imgList[5].closeItem' alt="账务管理">
             <span class="app-title">账务管理</span>
           </li>
           <li class="app-item" @click="goto('abnormalNotice/listDetail')">
-            <img src="../../../static/icon/ic_abnormity_notice.png" alt="异常提醒">
+            <img :src='flag?imgList[6].openItem:imgList[6].closeItem' alt="异常提醒">
             <span class="app-title">异常提醒</span>
           </li>
-          <!--<li class="app-item" @click="goto('examples')">-->
-            <!--<img src="../../../static/icon/ic_abnormity_notice.png" alt="异常提醒">-->
-            <!--<span class="app-title">组件构造</span>-->
-          <!--</li>-->
         </ul>
       </Group>
+      <Group title="待办事项" v-if="isHaveTodoList">
+        <Cell v-if="prepayTodoNum > 0" icon="../../../static/icon/ic_prepay_confirm.png" title="订单金额待确认" link :badge="prepayTodoNum"
+              @onClick="goto('prepay/0')"></Cell>
+        <Cell v-if="identityNum > 0" icon="../../../static/icon/ic_police.png" title="您有一条公安验证待处理" link :badge="identityNum"
+              @onClick="goto('/identity/0')"></Cell>
+        <Cell v-if="policeIdentityNum > 0" icon="../../../static/icon/ic_lvye.png" title="旅业信息待上传" link :badge="policeIdentityNum"
+              @onClick="goto('policeIdentity/handle/0')"></Cell>
+        <Cell v-if="invoiceNum > 0" icon="../../../static/icon/ic_invoice.png" title="开票申请待处理" link :badge="invoiceNum"
+              @onClick="goto('invoice/0')"></Cell>
+        <Cell v-if="checkoutApplicationNum > 0" icon="../../../static/icon/ic_checkout.png" title="离店申请待处理" link :badge="checkoutApplicationNum"
+              @onClick="goto('receive/checkout-application')"></Cell>
+        <Cell v-if="abnormalNoticeNum > 0" icon="../../../static/icon/ic_abnormity_notice.png" title="异常提醒" link :badge="abnormalNoticeNum"
+              @onClick="goto('abnormalNotice/listDetail')"></Cell>
+      </Group>
+      <div v-else class="none-list-container">
+        <img src="../../../static/icon/no_todo_list.png" alt="">
+        <span>暂无待办事项</span>
+      </div>
     </div>
   </div>
 </template>
@@ -70,13 +62,44 @@
     name: 'home',
     data() {
       return {
+        flag:false,
         List: [],
         prepayTodoNum: 0,
         identityNum: 0,
         policeIdentityNum: 0,
         invoiceNum: 0,
         checkoutApplicationNum: 0,
-        abnormalNoticeNum: 0
+        abnormalNoticeNum: 0,
+        imgList:[
+          {
+            openItem:'../../../static/icon/ic_police.png',
+            closeItem:'../../../static/icon/ic_police.png'
+          },
+          {
+            openItem:'../../../static/icon/ic_bill.png',
+            closeItem:'../../../static/icon/closeBill.png'
+          },
+          {
+            openItem:'../../../static/icon/ic_checkout.png',
+            closeItem:'../../../static/icon/closeCheckout.png'
+          },
+          {
+            openItem:'../../../static/icon/ic_Lvye.png',
+            closeItem:'../../../static/icon/closeLvye.png'
+          },
+          {
+            openItem:'../../../static/icon/ic_invoice.png',
+            closeItem:'../../../static/icon/closeInvoice.png'
+          },
+          {
+            openItem:'../../../static/icon/ic_bill.png',
+            closeItem:'../../../static/icon/closeBill.png'
+          },
+          {
+            openItem:'../../../static/icon/ic_abnormity_notice.png',
+            closeItem:'../../../static/icon/closeAbnormity.png'
+          }
+        ]
       }
     },
     computed: {
