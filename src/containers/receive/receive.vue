19c1d295ed27d8b83c854c972e79511031db8394
sunshujing <sunshujing@fortrun.cn>
2018-02-28 11:10:34 +0800

    旅业上传失败

--- src/containers/receive/receive.vue
+++ src/containers/receive/receive.vue
@@ -334,7 +334,7 @@
               dom += `<div style="color: #4a4a4a;justify-content: space-between;line-height: 2;">
                     <p><span style="color:  #8A8A8A">入住人</span>`
               i.checkin_status && i.checkin_status === 'R' ? dom += ` <span style="float:right;margin-left:5px;">(尚未入住)</span>` : null
-              i.checkin_status && i.checkin_status === 'I' && i.lvye_report_in_status && i.lvye_report_in_status === 'FAILED' ? dom += ` <span style="float:right;margin-left:5px;color:#FFB01F;">()</span>` : null
+              i.checkin_status && i.checkin_status === 'I' && i.lvye_report_in_status && i.lvye_report_in_status === 'FAILED' ? dom += ` <span style="float:right;margin-left:5px;color:#FFB01F;">(旅业上传失败)</span>` : null
               i.checkin_status && i.checkin_status === 'I' && i.lvye_report_in_status && i.lvye_report_in_status === 'SUCCESS' ? dom += ` <span style="float:right;margin-left:5px;color:#86E85E;">(旅业已上报)</span>` : null
               i.checkin_status && i.checkin_status === 'I' && i.lvye_report_in_status && i.lvye_report_in_status === 'PENDING ' ? dom += ` <span style="float:right;margin-left:5px;">(旅业正在上传)</span>` : null
               dom += `<span style="float: right">${i.name}</span>`;
