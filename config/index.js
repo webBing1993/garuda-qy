a1614039f8767f6628363f7bc8510dd0d62740e7
Zoe <zoe.zheng@fortrun.cn>
2018-03-23 11:46:14 +0800

    改地址

--- config/index.js
+++ config/index.js
@@ -28,7 +28,7 @@ module.exports = {
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     proxyTable: {
-      '/s/master/': {
+      '/i/master/': {
         target: 'https://wqt.fortrun.cn/',
         changeOrigin: true,
         pathRewrite: {
