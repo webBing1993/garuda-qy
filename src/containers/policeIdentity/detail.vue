7db3f01a8dc1215252d007fdeab64c334ab71f84
zoe <1423736181@qq.com>
2018-05-15 14:31:49 +0800

    值房通fix预登记状态2

--- src/containers/policeIdentity/detail.vue
+++ src/containers/policeIdentity/detail.vue
@@ -40,7 +40,7 @@
           <div v-if="item.orderOpen">
             <p class="orderItem">
               <span class="titleInfo">订单号：</span><span>{{item.order_no}}</span>
-              <span class="roomStatus" @click="(confirmOrderStatus=true,checkIndex=0,checkItem=item)">{{item.precheckin_status==1?'未确认':item.pay_mode==1?'房费现付':'不需房费现付'}}<i v-if="item.precheckin_status==6" class="iconfont icon-huodongbiaoqian"></i></span>
+              <span class="roomStatus" @click="(confirmOrderStatus=true,checkIndex=0,checkItem=item)">{{item.precheckin_status==1?'未确认':item.pay_mode==1?'不需房费现付':'房费现付'}}<i v-if="item.precheckin_status==6" class="iconfont icon-huodongbiaoqian"></i></span>
             </p>
             <div class="line"></div>
             <p class="orderItem">
@@ -168,7 +168,7 @@
         guestTypelist:[{key: 'LODGER', value: '住客'}, {key: 'VISITOR', value: '访客'},{key: 'STAFF', value: '酒店工作人员'}],
           //值房通状态数据
           checkIndex:0,
-          statusList:[{name:'房费现付',value:1},{name:'不需现付房费',value:2}],
+          statusList:[{name:'不需现付房费',value:1},{name:'房费现付',value:2}],
           confirmOrderStatus:false,
           orderStatus:0,
           // orderOpen:true,
