dd0b0a804b37a5c1f3d5c2efe776c2b46ed52d39
zoe <1423736181@qq.com>
2018-05-21 14:45:08 +0800

    订单中心改状态fix2

--- src/containers/prepay/list.vue
+++ src/containers/prepay/list.vue
@@ -259,7 +259,7 @@
         i: false,
         confirmOrderStatus:false,
         checkIndex:0,
-        statusList:[{name:'不需现付房费',value:1},{name:'房费现付',value:2}],
+        statusList:[{name:'房费现付',value:1},{name:'不需现付房费',value:2}],
         payMode:1,
         freeDeposit:false,
         checkItem:{},
