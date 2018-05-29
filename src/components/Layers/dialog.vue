cbf579297295dcff401749b3afd455e772e94cae
zoe <1423736181@qq.com>
2018-05-29 09:58:01 +0800

    发布并隐藏充值中心app

--- src/components/Layers/dialog.vue
+++ src/components/Layers/dialog.vue
@@ -61,12 +61,12 @@
   @weuiDialogLinkActiveBc: #EEEEEE;
   @weuiDialogGapWidth: 1.6em;
   @weuiTextColorGray: #4a4a4a;
-    .UIMask{
+    .UImask{
         position: fixed;
         z-index: 1000;
         top: 0;
         right: 0;
-        left: 50%;
+        left: 0;
         bottom: 0;
         background: rgba(0, 0, 0, .6);
     }
