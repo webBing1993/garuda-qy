3b25e7d7be74ba26146ba7bb42c5bdc5fff5e2e8
zoe <1423736181@qq.com>
2018-05-31 19:40:07 +0800

    隐藏剩余次数3

--- src/containers/policeIdentity/list.vue
+++ src/containers/policeIdentity/list.vue
@@ -204,11 +204,11 @@
       </Dialog>
     </div>
 
-    <div class="noCheckAlert">
-      <Dialog v-model="showAlert" title="提示" @onConfirm="showAlert=false" confirm confirmVal="确定">
-        <div>金额不足暂无法使用，请及时充值！</div>
-      </Dialog>
-    </div>
+    <!--<div class="noCheckAlert">-->
+      <!--<Dialog v-model="showAlert" title="提示" @onConfirm="showAlert=false" confirm confirmVal="确定">-->
+        <!--<div>金额不足暂无法使用，请及时充值！</div>-->
+      <!--</Dialog>-->
+    <!--</div>-->
     <!--////////////////////弹窗部分-->
   </article>
 </template>
