d89036a793a66013150c771294d161bc0c403e64
zoe <1423736181@qq.com>
2018-06-29 15:38:21 +0800

    人证通弹框处理

--- src/containers/policeIdentity/list.vue
+++ src/containers/policeIdentity/list.vue
@@ -126,7 +126,7 @@
 
     <!--无证核验弹窗-->
     <div class="nocheckDialogs">
-      <Dialog v-model="without_license" @onConfirm="makeSureVerify" confirm cancel cancelVal="取消" confirmVal="确定" :isDisabled="!validateNoIdCard">
+      <Dialog :maskHide=false v-model="without_license" @onConfirm="makeSureVerify" confirm cancel cancelVal="取消" confirmVal="确定" :isDisabled="!validateNoIdCard">
         <div class="withoutLicenseCon">
           <div class="title">无证核验</div>
           <group>
@@ -163,13 +163,13 @@
         </div>
       </Dialog>
     </div>
-    <Dialog v-model="showAlert" title="" @onConfirm="showAlert=false" confirm confirmVal="确定">
+    <Dialog  v-model="showAlert" title="" @onConfirm="showAlert=false" :maskHide=false confirm confirmVal="确定">
       <icon type="warn"></icon>
       <div style="margin:1rem 0;text-align: left">金额不足,暂无法使用，请联系旅业公司！</div>
     </Dialog>
-    <Dialog v-model="showIdcardAlert" title="" @onConfirm="showIdcardAlert=false" confirm confirmVal="确定">
+    <Dialog  v-model="showIdcardAlert" title="" @onConfirm="showIdcardAlert=false;without_license=true" :maskHide=false confirm confirmVal="确定">
       <icon type="warn"></icon>
-      <div style="margin:2rem 0">身份证位数少于18位！</div>
+      <div style="margin:2rem 0">证件号不是二代身份证号码！</div>
     </Dialog>
   </article>
 </template>
@@ -424,7 +424,7 @@
                 if(this.validateNoIdCard){
                     let len=this.idCard.split('').length;
                     if(len<18){
-                        this.without_license=true;
+                        // this.without_license=true;
                         this.showIdcardAlert=true;
                     } else if(len==18){
                         this.withoutIdCard({
